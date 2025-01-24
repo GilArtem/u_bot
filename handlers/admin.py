d7476e5a05abdbea6667fc765a67d575d8bcae4b
Gil Artem <gilart97@gmail.com>
2025-01-24 17:09:32 +0300

    Готов к отправке
    Реализован функционал списания средств через qr

--- handlers/admin.py
+++ handlers/admin.py
@@ -57,11 +57,10 @@ async def debit_amount_chosen(message: Message, state: FSMContext):
         return
     
     if user.balance >= amount:  
-        await safe_send_message(bot, user_id, text=f'Администратор запросил списание {amount} рублей.\nПодтвердите операцию?')
-        # ПЕРЕЛОМ 
-        # Сохраняем данные для пользователя
-        await state.update_data(amount=amount)
-        await state.set_state(AdminActions.waiting_debit_confirmation)
+        await safe_send_message(bot, user_id, text='Внимание: Возврат средств после списания невозможен')
+        await safe_send_message(bot, user_id, text=f'Администратор запросил списание {amount} рублей.\nПодтвердите операцию?', reply_markup=user_selection_button())
+        await create_transaction(user_id, admin_id, amount) 
+        await state.clear() 
     else:
         await safe_send_message(bot, message, text='Недостаточно средств на балансе.')
         await state.clear()
